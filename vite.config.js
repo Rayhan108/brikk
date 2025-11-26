8e6164073909d5d6591d921d1c74a105ae558261
rayhan108 <raihanshorker56@gmail.com>
2025-11-26 12:48:29 +0600

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For development server
+  server: {
+    host: true, // accept connections from network IPs
+    port: 5173, // optional: default Vite port
+  },
+ 
+  // For preview after build
+  preview: {
+    host: '0.0.0.0',  // allow external access
+    port: 4173,        // preview port
+    strictPort: false, // if true, will fail if port is taken
+  }
 })
