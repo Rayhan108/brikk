aac58e79f41d5f889c6d29649ffbfdb1726b05d9
rayhan108 <raihanshorker56@gmail.com>
2025-11-27 15:50:14 +0600

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   // For development server
   server: {
-    host: true, 
-    port: 5173, 
+    host: "dashboard.brikky.net",  // Set the host to your desired domain
+    port: 5173,
   },
 
   // For preview after build
   preview: {
-    host: "0.0.0.0", 
-    port: 4173, 
+    host: "dashboard.brikky.net",  // Set the host to your desired domain
+    port: 4173,
     strictPort: false,
   },
 });
